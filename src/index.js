59a4fdcefc53edec3b34df2ae3ecd3fc0ee2aced
Harrison Anthony <harrisoneanthony@gmail.com>
2023-04-05 13:48:21 -0400

    simple counter app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
+import './style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Action creator
+function incrementCounter(num) {
+  return { type: 'INCREMENT', num: num }
+}
+
+const initialState = {
+  count: 0
+};
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+    default:
+      return state;
+  }
+}
+
+function Counter(props) {
+  function handleClick() {
+    props.incrementCounter(1);
+  }
+    return <div>
+    <p>{props.count}</p>
+    <button onClick={handleClick}>Increment</button>
+    </div>;
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+const mapDispatchToProps = {
+  incrementCounter
+}
+
+const store = createStore(reducer);
+
+const Count = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const el = <Provider store={store}>
+          <Count/>
+        </Provider>; 
+
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
