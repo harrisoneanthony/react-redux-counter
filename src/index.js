6a06297f9fef4d7c5fec6c53794ca9c5a56aea4f
Harrison Anthony <harrisoneanthony@gmail.com>
2023-04-05 19:30:04 -0400

    project structure update: counter.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { connect } from 'react-redux';
+import Counter from './Counter';
 import './style.css';
-
-// Action creator
-function incrementCounter(num) {
-  return { type: 'INCREMENT', num: num }
-}
 
 const initialState = {
   count: 0
@@ -23,31 +18,9 @@ function reducer(state = initialState, action) {
   }
 }
 
-function Counter(props) {
-  function handleClick() {
-    props.incrementCounter(1);
-  }
-    return <div>
-    <p>{props.count}</p>
-    <button onClick={handleClick}>Increment</button>
-    </div>;
-}
-
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  };
-}
-const mapDispatchToProps = {
-  incrementCounter
-}
-
 const store = createStore(reducer);
-
-const Count = connect(mapStateToProps, mapDispatchToProps)(Counter);
-
 const el = <Provider store={store}>
-          <Count/>
+          <Counter/>
         </Provider>; 
 
 ReactDOM.render(
